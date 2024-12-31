fa0169048f82371fa5677cd5fad8a162d7d2c7f2
Parth Maru <theparthmaru@gmail.com>
2024-12-31 21:28:38 +0000

    Added routing

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use("/api/contacts", require("./routes/contactRoutes"));
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
-});
-
-app.get("/api/contacts", (req, res) => {
-	res.status(200).json({ message: "Get all contacts" });
 });
