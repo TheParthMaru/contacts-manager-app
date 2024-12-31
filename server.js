dbc1caec77fc60bbd8e079b66af54a4f557e4e67
Parth Maru <theparthmaru@gmail.com>
2024-12-31 21:18:52 +0000

    Added get all

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
 });
+
+app.get("/api/contacts", (req, res) => {
+	res.status(200).json({ message: "Get all contacts" });
+});
